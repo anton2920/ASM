8bda565259ed75540f95834702ba096e291b8fdc
anton2920 <anton2920@gmail.com>
2019-09-26 00:51:32 +0300

    PC: add polya_saod (lab_02)

---
+++ experiments/polya_saod/constants.s
@@ -0,0 +1,68 @@
+# For constants definitions
+.equ LIBC_ENABLED, 1
+
+.equ STD_PERMS, 0666
+.equ O_RDONLY, 0
+.equ O_RDWR, 2
+
+.equ STDOUT, 1
+.equ STDIN, 0
+.equ STDERR, 2
+
+.equ sizeof_int, 4
+.equ first_arg, sizeof_int + sizeof_int
+.equ second_arg, first_arg + sizeof_int
+.equ third_arg, second_arg + sizeof_int
+
+.equ SEEK_SET, 0
+.equ SEEK_CUR, 1
+.equ SEEK_END, 2
+
+# Database
+# struct db {
+#	int hash; /* header */
+#	int lastId; /* header */
+#	struct group grList[];
+# }
+
+.equ HEADER_SIZE, sizeof_int + sizeof_int
+
+.equ STRUCT_SIZE, 21
+.equ NAME_SIZE, 8
+.equ iD, 0
+.equ NAME, iD + sizeof_int
+.equ YEAR, NAME + NAME_SIZE
+.equ QUANT, YEAR + sizeof_int
+.equ FLAG, QUANT + sizeof_int
+# struct group {
+#	int id;
+#	char NAME[NAME_SIZE];
+#	int YEAR;
+#	int QUANT;
+#	bool FLAG;
+# };
+
+.equ sizeof_bool, 1
+.equ false, 0
+.equ true, 1
+# typedef enum {
+#	false,
+#	true	
+# } bool;
+
+.equ INT_MAX_LEN, 10
+.equ YEAR_MAX_LEN, 5
+.equ GR_SIZE_MAX_LEN, 3
+
+.equ SYS_MMAP, 90
+.equ SYS_MUNMAP, 91
+
+.equ PROT_NONE, 0x0
+.equ PROT_READ, 0x1
+.equ PROT_WRITE, 0x2
+.equ PROT_EXEC, 0x4
+
+.equ PROT_RDWR, 0x3 # PROT_READ | PROT_WRITE
+
+.equ MAP_SHARE, 0x1
+.equ MAP_PRIVATE, 0x2
---
+++ experiments/polya_saod/main.s
@@ -0,0 +1,182 @@
+.include "constants.s"
+
+.section .rodata
+file_name:
+	.asciz "test.txt"
+out_name:
+	.asciz "out.txt"
+error_msg:
+	.asciz "Some error occured!\n"
+	.equ len_error_msg, . - error_msg
+
+.section .bss
+.equ BUFSIZE, 500
+.lcomm BUF, BUFSIZE
+
+.section .text
+.globl _start
+_start:
+	# Initializing stack frame
+	movl %esp, %ebp
+	.equ fd, -4 # int
+	.equ fd_out, -8 # int
+	subl $0x8, %esp # Acquiring space for two variables
+
+	# Initializing variables
+
+	# Main part
+	pushl $STD_PERMS
+	pushl $O_RDONLY
+	pushl $file_name
+	call open
+	addl $0xC, %esp
+
+	testl %eax, %eax
+	js error
+
+	movl %eax, fd(%ebp)
+
+	pushl $STD_PERMS
+	pushl $O_RDWR
+	pushl $out_name
+	call open
+	addl $0xC, %esp
+
+	testl %eax, %eax
+	js error
+
+	movl %eax, fd_out(%ebp)
+
+	pushl fd(%ebp)
+	pushl $BUFSIZE
+	pushl $BUF
+	call getline
+	addl $0xC, %esp
+
+	pushl $BUFSIZE
+	pushl $BUF
+	pushl fd_out(%ebp)
+	call write
+	addl $0xC, %esp
+
+	pushl fd_out(%ebp)
+	pushl $'\r'
+	call lputc
+	addl $0x8, %esp
+
+main_loop:
+	pushl fd(%ebp)
+	pushl $BUFSIZE
+	pushl $BUF
+	call getline
+	addl $0xC, %esp
+
+	testl %eax, %eax
+	jz main_loop_end
+
+	pushl $BUF
+	call lstrlen
+	addl $0x4, %esp
+
+	pushl fd_out(%ebp)
+	pushl %eax
+	pushl $BUF
+	call write
+	addl $0xC, %esp
+
+	pushl fd_out(%ebp)
+	pushl $'\n'
+	call lputc
+	addl $0x8, %esp
+
+	pushl $0x0
+	pushl $SEEK_SET
+	pushl fd_out(%ebp)
+	call lseek
+	addl $0xC, %esp
+
+	jmp main_loop
+
+main_loop_end:
+	pushl fd(%ebp)
+	call close
+	addl $0x4, %esp
+
+	pushl fd_out(%ebp)
+	call close
+	addl $0x4, %esp
+
+	jmp exit
+
+error:
+	pushl $len_error_msg
+	pushl $error_msg
+	pushl $STDERR
+	call write
+	addl $0xC, %esp
+
+exit:
+	xorl %eax, %eax
+	incl %eax
+	xorl %ebx, %ebx
+	int $0x80 # 0x80's interrupt
+
+.type getline, @function
+getline:
+	# Initializing function's stack frame
+	pushl %ebp
+	movl %esp, %ebp
+
+	# Initializing variables
+	xorl %ecx, %ecx
+
+	# Main part
+getline_main_loop:
+	cmpl second_arg(%ebp), %ecx
+	jg getline_main_loop_end
+
+	pushl $0x1
+	movl first_arg(%ebp), %edx # Buf
+	leal (%edx, %ecx), %eax
+	pushl %eax
+	pushl third_arg(%ebp)
+	call read
+	addl $0xC, %esp
+
+	testl %eax, %eax
+	jz getline_main_loop_end
+
+	cmpb $0xA, (%edx, %ecx)
+	je getline_main_loop_end
+
+	incl %ecx
+
+	jmp getline_main_loop
+
+getline_main_loop_end:
+	# Destroying function's stack frame
+	movl %ebp, %esp
+	popl %ebp
+	retl
+
+.type lputc, @function
+lputc:
+	# Initializing function's stack frame
+	pushl %ebp
+	movl %esp, %ebp
+
+	# Initializing variables
+	leal first_arg(%ebp), %eax
+	movl second_arg(%ebp), %ebx
+
+	# I/O flow
+	pushl $0x1
+	pushl %eax # &a
+	pushl %ebx
+	call write
+	addl $0xC, %esp
+
+	# Destroying function's stack frame
+	movl %ebp, %esp
+	popl %ebp
+	retl
