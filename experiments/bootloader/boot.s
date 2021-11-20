83f9d51b8398595670d8ab7911f891f05b6b58f7
anton2920 <anton2920@gmail.com>
2021-11-20 14:43:44 +0300

    PC: updated bootloader

--- experiments/bootloader/boot.s
+++ experiments/bootloader/boot.s
@@ -3,36 +3,26 @@
 .section .text
 .globl _start
 _start:
-	# Setting screen mode (16/8-color, 80x25 EGA: 64-color)
-	movb $0x00, %ah
-	movb $0x13, %al
-	int $0x10
-
 	# Writing characters
-	movb $0x9, %ah # Write attribute and charater at cursor position
-	movw $hello_str, %dx # Our loop counter
-	movb $0x1, %bh # Using page #1
-	movb $0x1, %bl # Using initial color of Blue
-	movw $0x1, %cx # Printing every character only once
+	movb $0xE, %ah # Teletype write to active page
+	leaw hello_str, %si # String buffer for 'lods'
+	cld
 
 _start_print_loop:
-	movb (%edx), %al
+	lodsb
 	testb %al, %al
 	jz _start_print_loop_end
 
 	int $0x10
 
-	incb %bl # Next colour
-	incw %dx # Next character
 	jmp _start_print_loop
 
 _start_print_loop_end:
-	int 0 # halt
+	hlt
 
 # .section .rodata
 hello_str:
-	.asciz "Hello, world!"
-	.equ hello_str_len, . - hello_str
+	.asciz "Hello, world!\n"
 
 # Zero padding
 .org 510
